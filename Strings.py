c4a3b0af631fee240fd0ea3483c0f9156b14d7fb
sedx876 <swatkins876@gmail.com>
2021-08-24 11:24:16 -0400

    Added repo

---
+++ Strings.py
@@ -0,0 +1,19 @@
+course = "Python's Course for Beginners"
+print(course)
+
+what_course = 'Python for "Beginners"'
+print(what_course)
+
+email = '''
+Hi John,
+
+Here is our first email to you!!!
+
+Thank you,
+The Support Team
+
+'''
+print(email)
+#index from a string
+print(course[5])
+print(course[0:3])
---
+++ app.py
@@ -0,0 +1,33 @@
+print('Sharon Watkins')
+print('o----')
+print(' ||||')
+print('*' * 10)
+price = 10
+rating = 4.9
+name = 'Sharon'
+is_published = False
+print(price)
+print(rating)
+print(name)
+print(is_published)
+name = 'John Smith'
+age = 20
+new_patient = True
+print(name)
+print(age)
+print(new_patient)
+name = input('What is your name? ')
+print('Hi ' + name)
+name = input('What is your name? ')
+color = input('What is your favorite color? ')
+print(name + ' likes ' + color)
+birth_year = input('Birth Year: ')
+print(type(birth_year))
+#type conversion
+age = 2021 - int(birth_year)
+print(age)
+
+weight_lbs = input('Weight in lbs: ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
+
