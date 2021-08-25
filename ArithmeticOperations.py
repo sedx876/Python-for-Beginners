25e270677156f65e694ecc30a6925e4420a22432
sedx876 <swatkins876@gmail.com>
2021-08-25 08:37:27 -0400

    arithmetic operations

---
+++ ArithmeticOperations.py
@@ -0,0 +1,28 @@
+import math
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+#can give back float
+print(10 / 3)
+#gives back whole number
+print(10 // 3)
+#gives remainder
+print(10 % 3)
+#exponential
+print(10 ** 3)
+
+x = 10
+#x = x + 3
+print(x)
+x += 3
+print(x)
+
+y = 2.9
+print(round(y))
+
+#absolute value
+print(abs(-2.9))
+
+#import math module for extra math methods
+print(math.ceil(2.9))
