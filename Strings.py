a995b4229f2d55adeaf60e7c901c11ac78095ec6
sedx876 <swatkins876@gmail.com>
2021-08-25 08:11:57 -0400

    More strings

--- Strings.py
+++ Strings.py
@@ -1,4 +1,4 @@
-course = "Python's Course for Beginners"
+course = "Python for Beginners"
 print(course)
 
 what_course = 'Python for "Beginners"'
@@ -17,3 +17,16 @@ print(email)
 #index from a string
 print(course[5])
 print(course[0:3])
+
+first = 'Sharon'
+last = 'Watkins'
+
+message = first + ' [' + last + '] is a coder'
+msg = f'{first} [{last}] is a coder'
+print(message)
+print(msg)
+print(len(course))
+print(course.upper())
+print(course.find('P'))
+print(course.replace('Beginners', 'Absolute Beginners').upper())
+print('Python' in course)
