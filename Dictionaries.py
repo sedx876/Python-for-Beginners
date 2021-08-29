63ce25fa294e73689a5887ec670b570c329d03ba
sedx876 <swatkins876@gmail.com>
2021-08-29 08:09:11 -0400

    Dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,10 @@
+#key value pairs
+customer = {
+    "name": "Sharon Watkins",
+    "age": 45,
+    "is_verified": True
+}
+print(customer)
+print(customer["name"])
+print(customer["age"])
+print(customer["is_verified"])
