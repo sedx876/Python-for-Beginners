feb1d14d5226d685155388bab9febd44efb9c5f1
sedx876 <swatkins876@gmail.com>
2021-08-29 07:27:04 -0400

    ListMethods

---
+++ ListMethods.py
@@ -0,0 +1,9 @@
+numbers = [5, 2, 1, 7, 4]
+numbers.insert(0, 10)
+numbers.remove(5)
+numbers.append(20)
+numbers.sort()
+numbers2 = numbers.copy()
+numbers2.reverse()
+print(numbers)
+print(numbers2)
